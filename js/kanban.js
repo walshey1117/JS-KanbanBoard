42c70bdc24f5fcae800635900e94c2dd3932ca97
Mark Walshe <m.walshe@sap.com>
2022-11-13 21:46:06 +0000

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -16,7 +16,41 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  let item = document.createElement('div');
+  item.classList.add('item');
+  item.id = 'item-${order}';
+  item.draggable = true;
+  item.addEventListener('dragstart', (event) => {
+    return event.dataTransfer.setData('text', event.target);
+  });
+  item.addEventListener('dragend', (event) => {
+    return event.clearData();
+  });
+  let input = document.createElement('input');
+  item.appendChild(input);
+  let save_btn = document.createElement('button');
+  save_btn.innerHTML = 'Save';
+  save_btn.addEventListener('click', () => {
+    save_btn.error = "";
+    if(input !== ""){
+      order += 1;
+      item.innerHTML = input;
+      adding = false;
+    }
+    else{
+      save_btn.error = input;
+    }
+  });
+  item.appendChild(save_btn);
+  return item;
 };
 
+
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const id = event.dataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(id));
+  });
+  element.addEventListener('drgover',(event) => event.preventDefault());
 });
